c764bc4644e5cd91e32a8de56bc4190837aa4c5f
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-06-09 14:34:29 +0700

    add while

--- ioFile.cpp
+++ ioFile.cpp
@@ -7,6 +7,22 @@ int main()
 {
     string baris;
 
-    ofstream outfile; //membuat objek output file stream
-    outfile.open("contoh file.txt"); //membuat file untuk ditulisi
+    ofstream outfile; //membuat objek output file stream (ofstream)
+    outfile.open("contoh file.txt"); //membuat file untuk ditulisi(outline)
+
+    cout << ">= Menulis file , \'q' untuk keluar" << endl;
+}
+
+int main()
+{
+    while (true)
+    {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q    
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam dile
+        outfile << baris << endl;
+    }
 }
