13a2d5bd57db087b4b2db5ebd79924f15ac1a087
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-06-09 15:15:21 +0700

    add catch

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -8,4 +8,7 @@ int main()
         throw 3.5;     //melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
+    catch(int a) {
+        cout << a << " : Pengecualian akan dieksekusi" << endl;  //pada blok ini akan dieksekusi
+    }
 }
